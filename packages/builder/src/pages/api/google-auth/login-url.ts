0cb07932e585b3f3d86305ce048c9ccad224c42e
Oleg <91701505+03leg@users.noreply.github.com>
2024-06-08 15:37:13 +0300

    Fix url

--- packages/builder/src/pages/api/google-auth/login-url.ts
+++ packages/builder/src/pages/api/google-auth/login-url.ts
@@ -1,6 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import { google_spreadsheets_scopes } from "~/constants";
 import { googleOAuthClient } from "~/constants/google-auth/googleOAuthClient";
+import { env } from "~/env.mjs";
 
 interface ResponseData {
   message: string;
@@ -15,7 +16,10 @@ export default function handler(
       access_type: "offline",
       prompt: "consent",
       scope: google_spreadsheets_scopes,
-      state: btoa(JSON.stringify({userId: req.query.userId})),
+      state: btoa(JSON.stringify({ userId: req.query.userId })),
+      ...(process.env.NODE_ENV === "production"
+        ? { redirect_uri: env.NEXTAUTH_URL + "/api/google-auth/callback" }
+        : {}),
     });
 
     res.redirect(url);
