8a745f1d01f34e2d555f297db2c7987033e6bac9
Oleg <91701505+03leg@users.noreply.github.com>
2024-08-05 10:56:45 +0300

    Change Dinosaurs config

--- packages/builder/docs/docusaurus.config.ts
+++ packages/builder/docs/docusaurus.config.ts
@@ -3,15 +3,15 @@ import type {Config} from '@docusaurus/types';
 import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
-  title: 'My Site',
+  title: 'KickoffBot',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://www.kickoffbot.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/content/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
