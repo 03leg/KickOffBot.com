5ceeec125e3ddeea1cd81539c03456e74ccd6850
Oleg <91701505+03leg@users.noreply.github.com>
2024-09-05 12:31:07 +0300

    Not clean connections

--- packages/builder/src/utils/getDbVersionProject.ts
+++ packages/builder/src/utils/getDbVersionProject.ts
@@ -2,7 +2,7 @@ import { BotProject } from "@kickoffbot.com/types";
 
 export function getDbVersionProject(json: string): string {
   const project = JSON.parse(json) as BotProject;
-  project.connections = [];
+  // project.connections = [];
 
   return JSON.stringify(project);
 }
