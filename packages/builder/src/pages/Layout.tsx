cf593fe71d32613b57e563a342cf7db7e9f52a94
Oleg <91701505+03leg@users.noreply.github.com>
2024-09-30 16:40:38 +0300

    Add Content-Security-Policy

--- packages/builder/src/pages/Layout.tsx
+++ packages/builder/src/pages/Layout.tsx
@@ -5,6 +5,7 @@ import { MainContent } from '~/components/app/MainContent';
 import { Header } from '~/components/app/Header';
 import { GoogleAnalytics } from '@next/third-parties/google'
 import { env } from '~/env.mjs';
+import Head from 'next/head';
 
 export default function Layout({ children }: React.PropsWithChildren) {
     const [open, setOpen] = React.useState(false);
@@ -15,6 +16,9 @@ export default function Layout({ children }: React.PropsWithChildren) {
 
     return (
         <Box sx={{ display: 'flex' }}>
+            <Head>
+                <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests"/>
+            </Head>
             <Header open={open} handleDrawerToggle={handleDrawerOpen} />
             <Drawer open={open} />
             <MainContent open={open}>{children}</MainContent>
