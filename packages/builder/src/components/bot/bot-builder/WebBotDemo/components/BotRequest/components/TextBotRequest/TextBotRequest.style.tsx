708b4da5430c34e29336a9808da261ff1af6b359
Oleg <91701505+03leg@users.noreply.github.com>
2024-10-14 13:35:31 +0300

    Use media query for request editors

--- packages/builder/src/components/bot/bot-builder/WebBotDemo/components/BotRequest/components/TextBotRequest/TextBotRequest.style.tsx
+++ packages/builder/src/components/bot/bot-builder/WebBotDemo/components/BotRequest/components/TextBotRequest/TextBotRequest.style.tsx
@@ -1,9 +1,16 @@
 import { makeStyles } from "tss-react/mui";
 
-export const useTextBoxRequestStyles = makeStyles()(({ spacing }) => ({
+export const useTextBoxRequestStyles = makeStyles()(({ spacing, breakpoints }) => ({
     root: {
         display: "flex",
-        alignItems: "center"
+        alignItems: "center",
+        width: '50%',
+        [breakpoints.down("md")]: {
+            width: "70%",
+        },
+        [breakpoints.down("sm")]: {
+            width: "calc(100% - 66px)",
+        },
     },
     textField: {
         flex: 1,
