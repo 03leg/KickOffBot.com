c910646acab49cfe6b68155cac29d2e89038ec54
Oleg <91701505+03leg@users.noreply.github.com>
2024-10-26 12:14:13 +0300

    Move components outside of pages

--- packages/builder/src/pages/theme-editor/[id]/index.tsx
+++ packages/builder/src/pages/theme-editor/[id]/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import dynamic from 'next/dynamic'
 
-const ThemeEditorComponent = dynamic(() => import('./ThemeEditor/ThemeEditor').then(mod => mod.default), {
+const ThemeEditorComponent = dynamic(() => import('../../../feature/theme/ThemeEditor/ThemeEditor').then(mod => mod.default), {
     ssr: false,
 });
 
