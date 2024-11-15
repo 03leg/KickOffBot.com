170fa7a601db4b035b202a2329ec653c65bf2091
Oleg <91701505+03leg@users.noreply.github.com>
2024-11-15 16:26:43 +0300

    Change scope

--- packages/builder/src/constants.ts
+++ packages/builder/src/constants.ts
@@ -3,6 +3,6 @@ export const EDIT_BOT_PATH = `${MY_BOTS_PATH}/edit/` as const;
 
 export const google_spreadsheets_scopes = [
     "https://www.googleapis.com/auth/userinfo.email",
-    "https://www.googleapis.com/auth/drive.readonly",
+    "https://www.googleapis.com/auth/drive.file",
     "https://www.googleapis.com/auth/spreadsheets",
 ];
